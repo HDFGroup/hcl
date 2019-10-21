d610610892fb7f8d13fec26c929cf5327320c223
Chris Hogan <c096h800@gmail.com>
2019-10-21 15:02:42 -0500

    Include Mercury installation

--- ci/install_libfabric.sh
+++ ci/install_libfabric.sh
@@ -9,5 +9,5 @@ LIBFABRIC_ARCHIVE=${LIBFABRIC_DIR}.tar.bz2
 wget https://github.com/ofiwg/libfabric/releases/download/v${LIBFABRIC_VERSION}/${LIBFABRIC_ARCHIVE}
 tar xf ${LIBFABRIC_ARCHIVE}
 pushd ${LIBFABRIC_DIR}
-./configure --prefix=$HOME/local && make && make install
+./configure --prefix=$HOME/local && make -j 2 && make install
 popd
