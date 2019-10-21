246166431a1ff68425d8d784a3e1e73c3a54f4d1
Chris Hogan <c096h800@gmail.com>
2019-10-21 14:51:42 -0500

    Add libfabric install script

---
+++ ci/install_libfabric.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Installing libfabric"
+
+LIBFABRIC_VERSION=1.8.1
+LIBFABRIC_DIR=libfabric-${LIBFABRIC_VERSION}
+LIBFABRIC_ARCHIVE=${LIBFABRIC_DIR}.tar.bz2
+
+wget https://github.com/ofiwg/libfabric/releases/download/v${LIBFABRIC_VERSION}/${LIBFABRIC_ARCHIVE}
+tar xf ${LIBFABRIC_ARCHIVE}
+pushd ${LIBFABRIC_DIR}
+./configure --prefix=$HOME/local && make && make install
+popd
