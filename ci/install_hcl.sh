914b119658efd7b03a4433d7a8e67505738e23e3
Chris Hogan <c096h800@gmail.com>
2019-10-21 16:25:28 -0500

    Add hcl install script

---
+++ ci/install_hcl.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+mkdir build
+pushd build
+
+CXXFLAGS="-I${LOCAL}/include"                                  \
+LDFLAGS="-L${LOCAL}/lib"                                       \
+    cmake                                                      \
+        -DCMAKE_INSTALL_PREFIX=${LOCAL}                        \
+        -DCMAKE_BUILD_RPATH="${LOCAL}/lib"                     \
+        -DCMAKE_INSTALL_RPATH="${LOCAL}/lib"                   \
+        -DCMAKE_BUILD_TYPE=Release                             \
+        -DCMAKE_CXX_COMPILER=`which mpicxx`                    \
+        -DCMAKE_C_COMPILER=`which mpicc`                       \
+        -DHCL_ENABLE_RPCLIB=${HCL_ENABLE_RPCLIB}               \
+        -DHCL_ENABLE_THALLIUM_TCP=${HCL_ENABLE_THALLIUM_TCP}   \
+        -DHCL_ENABLE_THALLIUM_ROCE=${HCL_ENABLE_THALLIUM_ROCE} \
+        -DBUILD_TEST=ON                                        \
+        ..
+
+cmake --build . -- -j 2 VERBOSE=1 && make install
