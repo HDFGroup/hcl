8b127283fdd735a0d6423f1cbe07eaa75d6f4a13
Hariharan Devarajan <mani.hariharan@gmail.com>
2020-12-04 16:04:14 -0600

    added install and run script.

--- ci/install_hcl.sh
+++ ci/install_hcl.sh
@@ -4,8 +4,9 @@ LOCAL=${HOME}/local
 mkdir build
 pushd build
 
-CXXFLAGS="-I${LOCAL}/include -fsanitize=address -O1 -fno-omit-frame-pointer -g" \
-LDFLAGS="-L${LOCAL}/lib"                                       \
+if [ "${HCL_ENABLE_RPCLIB}" = "ON" ]; then
+    CXXFLAGS="-I${LOCAL}/include -fsanitize=address -O1 -fno-omit-frame-pointer -g" \
+    LDFLAGS="-L${LOCAL}/lib"                                       \
     cmake                                                      \
         -DCMAKE_INSTALL_PREFIX=${LOCAL}                        \
         -DCMAKE_BUILD_RPATH="${LOCAL}/lib"                     \
@@ -19,9 +20,9 @@ LDFLAGS="-L${LOCAL}/lib"                                       \
         -DBUILD_TEST=ON                                        \
         ..
 
-cmake --build . -- -j 2 VERBOSE=1 || exit 1
+    cmake --build . -- -j 2 VERBOSE=1 || exit 1
 
-if [ "${HCL_ENABLE_RPCLIB}" = "ON" ]; then
+
     pushd test
     # TODO(chogan): Run tests via ctest and enable Thallium tests
     mpiexec -n 2 ./map_test || exit 1
@@ -31,19 +32,7 @@ if [ "${HCL_ENABLE_RPCLIB}" = "ON" ]; then
     mpiexec -n 2 ./set_test || exit 1
     mpiexec -n 2 ./unordered_map_test || exit 1
     popd
-fi
 
-if [ "${HCL_ENABLE_THALLIUM_TCP}" = "ON" ]; then
-    pushd test
-    # TODO(chogan): Run tests via ctest and enable Thallium tests
-    mpiexec -n 2 ./map_test || exit 1
-    mpiexec -n 2 ./multimap_test || exit 1
-    mpiexec -n 2 ./priority_queue_test || exit 1
-    mpiexec -n 2 ./queue_test || exit 1
-    mpiexec -n 2 ./set_test || exit 1
-    mpiexec -n 2 ./unordered_map_test || exit 1
-    popd
+    make install || exit 1
 fi
-
-make install || exit 1
 popd
