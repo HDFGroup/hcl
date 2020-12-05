e55dc9fd1825d230d4d1b1de1901a0133d2dd326
hariharan devarajan <mani.hariharan@gmail.com>
2020-12-04 19:57:35 -0600

    fixed ci

--- ci/install_hcl.sh
+++ ci/install_hcl.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
-LOCAL=${HOME}/local
+LOCAL=local
+INSTALL_DIR="${HOME}/${LOCAL}"
+SPACK_DIR=${INSTALL_DIR}/spack
+INSTALL_DIR=${SPACK_DIR}/var/spack/environments/hcl/.spack-env/view
 mkdir build
 pushd build
 
-CXXFLAGS="-I${LOCAL}/include -fsanitize=address -O1 -fno-omit-frame-pointer -g" \
-LDFLAGS="-L${LOCAL}/lib"                                       \
+if [ "${HCL_ENABLE_RPCLIB}" = "ON" ]; then
+    CXXFLAGS="-I${INSTALL_DIR}/include -fsanitize=address -O1 -fno-omit-frame-pointer -g" \
+    LDFLAGS="-L${INSTALL_DIR}/lib"                                       \
     cmake                                                      \
-        -DCMAKE_INSTALL_PREFIX=${LOCAL}                        \
-        -DCMAKE_BUILD_RPATH="${LOCAL}/lib"                     \
-        -DCMAKE_INSTALL_RPATH="${LOCAL}/lib"                   \
+        -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR}                        \
+        -DCMAKE_BUILD_RPATH="${INSTALL_DIR}/lib"                     \
+        -DCMAKE_INSTALL_RPATH="${INSTALL_DIR}/lib"                   \
         -DCMAKE_BUILD_TYPE=Release                             \
         -DCMAKE_CXX_COMPILER=`which mpicxx`                    \
         -DCMAKE_C_COMPILER=`which mpicc`                       \
@@ -19,9 +23,9 @@ LDFLAGS="-L${LOCAL}/lib"                                       \
         -DBUILD_TEST=ON                                        \
         ..
 
-cmake --build . -- -j 2 VERBOSE=1 || exit 1
+    cmake --build . -- -j 2 VERBOSE=1 || exit 1
 
-if [ "${HCL_ENABLE_RPCLIB}" = "ON" ]; then
+
     pushd test
     # TODO(chogan): Run tests via ctest and enable Thallium tests
     mpiexec -n 2 ./map_test || exit 1
@@ -31,7 +35,7 @@ if [ "${HCL_ENABLE_RPCLIB}" = "ON" ]; then
     mpiexec -n 2 ./set_test || exit 1
     mpiexec -n 2 ./unordered_map_test || exit 1
     popd
-fi
 
-make install || exit 1
+    make install || exit 1
+fi
 popd
