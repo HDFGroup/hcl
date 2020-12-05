c67f6d9717fb22336c6bb751f7a034ca8108daa6
hariharan devarajan <mani.hariharan@gmail.com>
2020-12-04 19:41:40 -0600

    add boost to the link folder.

--- ci/install_hcl.sh
+++ ci/install_hcl.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
-LOCAL=${HOME}/local
+LOCAL=local
+INSTALL_DIR="${HOME}/${LOCAL}"
+SPACK_DIR=${INSTALL_DIR}/spack
+INSTALL_DIR=${SPACK_DIR}/var/spack/environments/hcl/.spack-env/view
 mkdir build
 pushd build
 
 if [ "${HCL_ENABLE_RPCLIB}" = "ON" ]; then
-    CXXFLAGS="-I${LOCAL}/include -fsanitize=address -O1 -fno-omit-frame-pointer -g" \
-    LDFLAGS="-L${LOCAL}/lib"                                       \
+    CXXFLAGS="-I${INSTALL_DIR}/include -fsanitize=address -O1 -fno-omit-frame-pointer -g" \
+    LDFLAGS="-L${INSTALL_DIR}/lib"                                       \
     cmake                                                      \
-        -DCMAKE_INSTALL_PREFIX=${LOCAL}                        \
-        -DCMAKE_BUILD_RPATH="${LOCAL}/lib"                     \
-        -DCMAKE_INSTALL_RPATH="${LOCAL}/lib"                   \
+        -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR}                        \
+        -DCMAKE_BUILD_RPATH="${INSTALL_DIR}/lib"                     \
+        -DCMAKE_INSTALL_RPATH="${INSTALL_DIR}/lib"                   \
         -DCMAKE_BUILD_TYPE=Release                             \
         -DCMAKE_CXX_COMPILER=`which mpicxx`                    \
         -DCMAKE_C_COMPILER=`which mpicc`                       \
